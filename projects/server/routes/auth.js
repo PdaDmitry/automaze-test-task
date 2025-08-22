5c9c6e357c557a8af196284b72b0b7d5cb7f2664
PdaDmitry <pda.gazcomp@gmail.com>
2025-08-22 13:47:25 +0300

    Restoring client model with correct casing

--- projects/server/routes/auth.js
+++ projects/server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Client = require("../models/Client");
 
 const router = express.Router();
 
