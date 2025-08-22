0eeea197b0c2f468bf5bf665d012b7e3844b3b50
PdaDmitry <pda.gazcomp@gmail.com>
2025-08-22 13:27:08 +0300

    update auth.js

--- projects/server/routes/auth.js
+++ projects/server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 
 const jwt = require("jsonwebtoken");
-const Client = require("../models/Сlient");
+const Client = require("../models/client");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
