14681edaf91e3cef908eaef18f6c802ab5daa0cb
PdaDmitry <pda.gazcomp@gmail.com>
2025-08-22 13:45:51 +0300

    Removing client model to fix casing issue

--- projects/server/routes/auth.js
+++ projects/server/routes/auth.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-
 const jwt = require("jsonwebtoken");
-const Client = require("../models/client");
+
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
