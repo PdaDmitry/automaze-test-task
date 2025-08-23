f8dd9412402ea8b1f623afa75f5ad7927311b2d8
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2025-08-23 18:32:50 +0300

    delete vercel-api.js

--- node_modules/server/local.js
+++ node_modules/server/local.js
@@ -4,8 +4,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URL = process.env.MONGODB_URL;
-const MONGO_DB = process.env.MONGODB_DB;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server started on http://localhost:${PORT}`);
